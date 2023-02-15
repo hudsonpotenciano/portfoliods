4ae55bf7a52414eca4890a42af02c914929d1a96
Hudson Potenciano <hudson.potenciano@valtech.com>
2023-02-15 11:44:38 -0300

    languages background button fix

--- packages/react/src/atoms/XpCard/XpCard.stories.tsx
+++ packages/react/src/atoms/XpCard/XpCard.stories.tsx
@@ -9,4 +9,4 @@ export default {
     title: 'Atoms|XpCard',
 };
 
-export const XpCard1 = () => <XpCard XpLogo={require("../../assets/images/icons/angular-icon.png")} />;
+export const XpCard1 = () => <XpCard name='angular' onclick={() => { }} logo="https://images.ctfassets.net/iciuyzpdgf5z/2ydxmeswymUkxXt6D1uHF6/86fead655542451bd3939aaa90f9ecac/angular.png" />;
